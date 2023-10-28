4bbf509b081daa1d0edc5f8bd398f8016162c6e1
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2023-10-28 17:21:19 +0100

    Update Chapter1:C++ Basics.cpp

--- ROS/Chapter1:C++ Basics.cpp
+++ ROS/Chapter1:C++ Basics.cpp
@@ -10,4 +10,19 @@
         return 0;}
 6 - g++ -std=c++11 name.cpp -o name_compiled
 7 - ./name_compiled
-  
+8 - #include "rosbot_control/rosbot_class.h"
+    #include <ros/ros.h>
+    
+    int main(int argc, char **argv) {
+      ros::init(argc, argv, "rosbot_node");
+    
+      RosbotClass rosbot;
+      rosbot.move();
+    
+      float coordinate = rosbot.get_position(1);
+    
+      ROS_INFO_STREAM(coordinate);
+    
+      return 0;
+    }  
+
